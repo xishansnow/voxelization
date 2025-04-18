70637768b9b9fc4d4b15ba2beddb2a5d34435183
Xishansnow <xishansnow@gmail.com>
2025-04-18 20:46:52 +0800

    Delete build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /bigdata/Voxelization/voxelization
-# Build directory: /bigdata/Voxelization/voxelization/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(voxelizer_tests "/bigdata/Voxelization/voxelization/build/voxelizer_tests")
-set_tests_properties(voxelizer_tests PROPERTIES  _BACKTRACE_TRIPLES "/bigdata/Voxelization/voxelization/CMakeLists.txt;166;add_test;/bigdata/Voxelization/voxelization/CMakeLists.txt;0;")
